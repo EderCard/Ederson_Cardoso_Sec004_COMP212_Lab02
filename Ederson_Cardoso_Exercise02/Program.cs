84feb4ccd74be65a666796cc4d174bb474f0e57a
EderCard <edersoncard@gmail.com>
2020-01-26 16:35:25 -0500

    Add Exercise02

---
+++ Ederson_Cardoso_Exercise02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ederson_Cardoso_Exercise02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // String to tested
+            string stringTest = "This is to test whether the extension method count can return a right answer or not.";
+
+            // Start StringBuilder
+            StringBuilder sb = new StringBuilder(stringTest);
+
+            // Call StringBuilder extention method CountWords
+            int words = sb.CountWords(stringTest);
+
+            Console.WriteLine("String test: ");
+            Console.WriteLine(stringTest);
+            // Print result           
+            Console.WriteLine("The string tested has " + words + " words.");
+        
+        } // end Main
+    } // end class
+} // end namespace
---
+++ Ederson_Cardoso_Exercise02/StringBuilderExtension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ederson_Cardoso_Exercise02
+{
+    public static class StringBuilderExtension
+    {
+        /// <summary>
+        /// This method returns the number of words in a string using StringBuilder extension method
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="inputString"></param>
+        /// <returns></returns>
+        public static int CountWords(this StringBuilder str, string inputString)
+        {
+            // Create a StringBuilder object
+            StringBuilder builder = new StringBuilder(inputString);
+
+            // Cast builder object as string
+            string builderString = builder.ToString();
+
+            // Split builderString in words separated by space and store it in an array
+            string[] stringArray = builderString.Split(' ');
+
+            // Return the number of words as array length
+            return stringArray.Length;
+        } // end CountWords method
+    
+    } // end class
+} // end namespace
